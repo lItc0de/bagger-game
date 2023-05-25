2f071e93317128c19018d41db8664fe6d864f6a1
litc0de <litc0de@posteo.org>
2023-05-26 01:11:29 +0200

    fix device orientation permission for ios

--- src/scenes/loadingScene.ts
+++ src/scenes/loadingScene.ts
@@ -26,22 +26,22 @@ export class LoadingScene extends Scene {
     });
     loadingText.setOrigin(0.5, 0.5);
 
-    const startButton = this.add
-      .text(this.cameras.main.centerX, this.cameras.main.centerY, 'Start', {
-        font: '20px monospace',
-        color: '#ffffff',
-      })
-      .setOrigin(0.5)
-      .setPadding(10)
-      .setStyle({ backgroundColor: '#111' })
-      .setInteractive({ useHandCursor: true })
-      .on('pointerdown', this.startGame.bind(this))
-      .on('pointerover', () => startButton.setStyle({ fill: '#f39c12' }))
-      .on('pointerout', () => startButton.setStyle({ fill: '#FFF' }));
+    // const startButton = this.add
+    //   .text(this.cameras.main.centerX, this.cameras.main.centerY, 'Start', {
+    //     font: '20px monospace',
+    //     color: '#ffffff',
+    //   })
+    //   .setOrigin(0.5)
+    //   .setPadding(10)
+    //   .setStyle({ backgroundColor: '#111' })
+    //   .setInteractive({ useHandCursor: true })
+    //   .on('pointerdown', this.startGame.bind(this))
+    //   .on('pointerover', () => startButton.setStyle({ fill: '#f39c12' }))
+    //   .on('pointerout', () => startButton.setStyle({ fill: '#FFF' }));
   }
 
   startGame(): void {
-    this.button?.click();
+    // this.button?.click();
   }
 
   handleButtonClick = (): void => {
@@ -52,8 +52,8 @@ export class LoadingScene extends Scene {
       .then(() => {
         this.scene.start('main-scene');
       })
-      .catch(() => {
-        console.error('Device orientation error');
+      .catch((e) => {
+        console.error('Device orientation error', e);
       });
   };
 }
--- src/utils/deviceOrientation.ts
+++ src/utils/deviceOrientation.ts
@@ -13,3 +13,30 @@ export const initDeviceOrientation = async (): Promise<boolean> => {
   const res = await requestPermission();
   return res === 'granted';
 };
+
+// export const initDeviceOrientation = async (): Promise<boolean> =>
+//   await new Promise<boolean>((resolve, reject) => {
+//     const requestPermission = (
+//       DeviceOrientationEvent as unknown as DeviceOrientationEventiOS
+//     ).requestPermission;
+//     const iOS = typeof requestPermission === 'function';
+
+//     if (!iOS) {
+//       resolve(true);
+//       return;
+//     }
+
+//     requestPermission()
+//       .then(res => {
+//         console.log('hello', res);
+
+//         if (res === 'granted') resolve(true);
+//         else reject(new Error('Device orientation permission problem'));
+//       })
+//       .catch((e) => {
+//         console.log('nooo');
+//         console.error(e);
+
+//         reject(new Error('Device orientation permission problem'));
+//       });
+//   });
